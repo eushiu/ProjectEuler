24c8ffd9b3b6f89fe2e4f29579e244e8ffe3e0aa
Eugene Shiu <eugene.shiu@aonhewitt.com>
2017-06-03 16:11:54 -0700

    Adds Days 1 through 3

---
+++ EulerDay1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            for (int x = 1; x < 1000; x++)
+            {
+                if(x % 3 == 0 || x % 5 == 0)
+                {
+                    sum = sum + x;
+                }
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EulerDay3/EulerDay3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EulerDay3
+{
+    class Program
+    {
+        //The prime factors of 13195 are 5, 7, 13 and 29.
+        //What is the largest prime factor of the number 600851475143 ?
+        public static int nTHPrime(int x)
+        {
+            return 1;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
